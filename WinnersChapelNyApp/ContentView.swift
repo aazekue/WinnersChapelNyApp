8fae6cf78302e6018b8daa9232219d8843ef50dc
aazekue <aazekue@gmail.com>
2024-02-08 15:52:38 -0500

    Initial Commit

---
+++ WinnersChapelNyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WinnersChapelNyApp
+//
+//  Created by Abel Azekwu on 2/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WinnersChapelNyApp/WinnersChapelNyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinnersChapelNyAppApp.swift
+//  WinnersChapelNyApp
+//
+//  Created by Abel Azekwu on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WinnersChapelNyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
